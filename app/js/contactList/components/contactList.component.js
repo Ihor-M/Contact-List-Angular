a85351c4942bb37bf2722c241050b061267f5903
Ihor-M <4u0810@gmail.com>
2016-11-17 01:14:53 +0200

    check date

--- app/js/contactList/components/contactList.component.js
+++ app/js/contactList/components/contactList.component.js
@@ -14,7 +14,43 @@ function MainContactListController(ContactListFactory, Notification) {
 
     ContactListFactory.GetContactsList().get().$promise.then(function (response) {
         ctrl.contactList = response.allContacts;
+
+        var respLength = Object.keys(ctrl.contactList).length;
+        var i;
+        var currDate = new Date();
+        var currYear = currDate.getFullYear();
+
+        for (i = 0; i < respLength; i++) {
+            var dateStr = ctrl.contactList[i].birthday_date;
+            var birthDate = new Date(dateStr);
+            var birthDay = birthDate.getDate();
+            var birthMonth = birthDate.getMonth() + 1;
+            var birthdayCurrYear = currYear + '-' + birthMonth + '-' + birthDay;
+            var birthdayCurrYearDate = new Date(birthdayCurrYear);
+            var beforeFiveDays = new Date(birthdayCurrYear);
+            var beforeTenDays = new Date(birthdayCurrYear);
+            beforeFiveDays.setDate(beforeFiveDays.getDate() - 5);
+            beforeTenDays.setDate(beforeTenDays.getDate() - 10);
+
+            if (currDate >= beforeFiveDays && currDate <= birthdayCurrYearDate ) {
+                ctrl.contactList[i].beforeFive = 'true';
+
+            } else {
+                ctrl.contactList[i].beforeFive = 'false';
+            }
+
+            if (currDate >= beforeTenDays && currDate <= birthdayCurrYearDate ) {
+                ctrl.contactList[i].beforeTen =  'true';
+            } else {
+                ctrl.contactList[i].beforeTen = 'false';
+            }
+
+            if (ctrl.contactList[i].beforeFive == 'true') {
+                ctrl.contactList[i].beforeTen = 'false'
+            }
+        }
     });
+
 
     ctrl.deleteContact = function(id)
     {
@@ -39,5 +75,4 @@ function MainContactListController(ContactListFactory, Notification) {
             }
         })
     };
-
 }
